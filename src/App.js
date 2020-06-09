107479609b239178d9835bafb7557c93c1660330
Justin Rothweiler <justin.rothweiler@weareadaptive.com>
2020-06-09 14:49:27 -0400

    change button text

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Watch from './stopwatch.js';
 
 function App() {
+  let [countingIntervalId, setCountingIntervalId] = useState(-1);
+  let [lapTimes, setLapTimes] = useState([]);
+  let [isCounting, setIsCounting] = useState(false);
+
+  const startStop = function() {
+    setIsCounting(!isCounting);
+  }
+
   return (
     <div className="App">
-      <Watch startStop={startStop} resetLap={resetLap}></Watch>
+      <Watch isCounting={isCounting} startStop={startStop} resetLap={resetLap}></Watch>
     </div>
   );
 }
 
-function startStop(){
-  alert("start");
-}
+
 
 function resetLap(){
   alert("reset");
