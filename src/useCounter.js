91b503f9b50caf67569f865c8197782cdbd4b2de
test <test@test>
2020-06-17 15:59:39 -0400

    new architecture

--- src/useCounter.js
+++ src/useCounter.js
@@ -1,5 +1,7 @@
 import {useState, useRef, useEffect, useReducer} from 'react';
 
+
+
 let useStopWatch = () =>{
   let startTime = useRef(null);
   let millisecondsPaused = useRef(0);
@@ -16,13 +18,24 @@ let useStopWatch = () =>{
     
   };
 
-  function reducer(state, action) {
-    switch (action) {
+  function reducer(state, {type, payload}) {
+    switch (type) {
       case 'toggle':
-        return {isCounting: !state.isCounting};
         
+        
+          
+        
+        return {...state, lastStopwatchToggleTime: payload, 
+                isCounting: !state.isCounting, 
+                startTime: state.startTime===null?payload:state.startTime, 
+                millisecondsPaused: (state.startTime!==null&&state.isCounting===false)? (payload-state.lastStopwatchToggleTime) + state.millisecondsPaused : state.millisecondsPaused};
+       
+      case 'tick' :
+
+        return {... state, ellapsedTime: payload-state.startTime-state.millisecondsPaused}
+
       case 'reset':
-        return {...state, isCounting: state.isCounting, isInitial:true};
+        return {...state, startTime: null, lastStopwatchToggleTime: null, ellapsedTime:0, millisecondsPaused: 0 };
 
       
       default:
@@ -30,10 +43,22 @@ let useStopWatch = () =>{
     }
   }
 
-  let initialState = {isCounting : false, isInitial: true};
-  const [state, dispatch] = useReducer(reducer, initialState);
+  let initialState = {isCounting : false, startTime: null, lastStopwatchToggleTime: null, ellapsedTime: 0, millisecondsPaused:0};
+  const [state, internalDispatch] = useReducer(reducer, initialState);
+
   useEffect(() => {
-    if (isSecondTime.current) {
+    
+    if (state.isCounting) {
+      const token =  setInterval(()=>{internalDispatch({type:'tick', payload:Date.now()})}, 10);
+      return () => { clearInterval(token) };
+    }
+    
+ 
+  }, [state.isCounting])
+
+  
+  /*useEffect(() => {
+    
       let currentTime= Date.now();
       if (!state.isCounting) {
         console.log("if branch")
@@ -53,38 +78,42 @@ let useStopWatch = () =>{
           countingIntervalId.current = setInterval(getShowTime, 10);
       }
       lastStopwatchToggleTime.current = Date.now();
-    } else {
+    
       isSecondTime.current = true;
       console.log('test');
-    }
+    
     
   }, [state.isCounting]);
 
-  useEffect(() => {
+ useEffect(() => {
     if (state.isInitial) {
       setEllapsedTime(0);
       lastStopwatchToggleTime.current = 0;
       millisecondsPaused.current = 0;
       startTime.current = null;
     }
-  }, [state.isInitial])
+  }, [state.isInitial])*/
 
-  return  [{isCounting: state.isCounting,
-     ellapsedTime, 
-     isInitial: state.isInitial,
-    millisecondsPaused: millisecondsPaused.current, 
-    startTime: startTime.current}, dispatch] 
+  const dispatch = (type) => {
+    if (type === "tick") {
+      return;
+    }
+
+    internalDispatch({ type, payload: Date.now() });
+  };
+
+  return  [{isCounting: state.isCounting, ellapsedTime:state.ellapsedTime}, dispatch] 
 }
 const useLapsStopWatch = () => {
   let lastLapTime = useRef(0);
-  const [{ellapsedTime, isInitial, isCounting, millisecondsPaused, startTime}, dispatch] = useStopWatch();
+  const [{ellapsedTime, isCounting}, internalDispatch] = useStopWatch();
   
-  function lapReducer(state, action) {
-    switch (action) {
+  function lapReducer(state, {type, payload}) {
+    switch (type) {
       case 'lap':
-        let currentTime = Date.now();
-        let totalTime = currentTime - startTime - millisecondsPaused- lastLapTime.current;
-        return {lapTimes: [totalTime, ...state.lapTimes]}
+        const totalTime = state.lapTimes.reduce((a, b) => a + b, 0);
+
+        return {lapTimes: [(ellapsedTime - totalTime), ...state.lapTimes]}
       case 'reset':
         return { lapTimes: [] }
 
@@ -102,13 +131,17 @@ const useLapsStopWatch = () => {
     lastLapTime.current = lapState.lapTimes.reduce((acc, b) => acc + b, 0)
   }, [lapState.lapTimes]);
 
+  const dispatch = (type) => {
+    if (type === "tick") {
+      return;
+    }
+
+    lapDispatch({type, payload: Date.now()});
+    internalDispatch(type);
+  };
   
   return [{isCounting: isCounting,
-    ellapsedTime, 
-    isInitial: isInitial, lapTimes:lapState.lapTimes}, (action) => {
-      lapDispatch(action);
-      dispatch(action);
-    }];
+    ellapsedTime, lapTimes:lapState.lapTimes}, dispatch];
 }
 
 export default useLapsStopWatch;
